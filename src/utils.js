431d60a6602fca5e151d76c46859e889612d592a
Nimira43 <n.rabone72@aol.com>
2025-01-04 16:58:04 +0000

    getElement takes a selection parameter, which is a CSS selector string. It uses the document.querySelector method to find the first element that matches this selector. If an element is found, it is returned. If no such element exists, the function throws an error with a message advising the user to check the selector.

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
-export const getElement = 'Get Element - working'
+const getElement = (selection) => {
+  const element = document.querySelector(selection)
+  if (element) return element
+  
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  )
+}
